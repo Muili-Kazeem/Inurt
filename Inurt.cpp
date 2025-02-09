4c4b0e0b6a2a259decfcd41205b187c391f0d94f
Muili Kazeem <muiliquazeem@gmail.com>
2025-02-09 10:50:15 +0100

    Add project files.

---
+++ Inurt.cpp
@@ -0,0 +1,30 @@
+// Inurt.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main()
+{
+    // Writing to files
+    /* 
+        std::string filename;
+        std::cin >> filename;
+
+        std::ofstream file(filename, std::ios::app);
+        file << "This is the start of the file";
+    */
+
+    // Multidimensional Arrays and Nested Vectors
+    // 
+    // Multimensional Arrays
+    // declared like so: int grades[][n] = {{2, 3, 4}, {34, 53, 21} {3, 4, 54}}
+    // The inner size of the array can be inferred but the outer dimension has to be explicitedly stated as seen indicated by 'n'
+    // 
+    // Same can be done with vectors and they are like so
+    // std::vector<std::vector<int>> grades = {{2, 3, 4}, {34, 53, 21} {3, 4, 54}}
+
+
+    return 0;
+}
